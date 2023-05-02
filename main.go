1b1f2e9b406366f85adb468108d40da0d4014ede
Sebastian Widmer <sebastian.widmer@vshn.net>
2023-05-02 16:57:18 +0200

    Don't collide with deprecated ocp-drain-monitor

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "3087d8b1.appuio.io",
+		LeaderElectionID:       "9fced507.appuio.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
