fa57b1415cfdbeab61c6f4ff62bf141b8f0f9e4c
Sebastian Widmer <sebastian.widmer@vshn.net>
2023-05-15 10:07:01 +0200

    Check MachineConfigPools to see if upgrade complete (#11)

--- pkg/healthcheck/healthcheck_test.go
+++ pkg/healthcheck/healthcheck_test.go
@@ -1,11 +1,16 @@
 package healthcheck_test
 
 import (
+	"fmt"
 	"testing"
+
+	machineconfigurationv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/appuio/openshift-upgrade-controller/pkg/healthcheck"
 	configv1 "github.com/openshift/api/config/v1"
-	"github.com/stretchr/testify/assert"
 )
 
 func Test_IsOperatorDegraded(t *testing.T) {
@@ -40,4 +45,53 @@ func Test_IsOperatorDegraded(t *testing.T) {
 		subject.Status.Conditions = nil
 		assert.False(t, IsOperatorDegraded(subject))
 	})
+}
+
+func Test_MachineConfigPoolsUpdating(t *testing.T) {
+	subject := machineconfigurationv1.MachineConfigPoolList{
+		Items: []machineconfigurationv1.MachineConfigPool{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "master",
+				},
+				Status: machineconfigurationv1.MachineConfigPoolStatus{
+					MachineCount:        3,
+					UpdatedMachineCount: 2,
+				},
+			}, {
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "worker",
+				},
+				Status: machineconfigurationv1.MachineConfigPoolStatus{
+					MachineCount:        3,
+					UpdatedMachineCount: 3,
+				},
+			}, {
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "x-storage",
+				},
+				Status: machineconfigurationv1.MachineConfigPoolStatus{
+					MachineCount:        3,
+					UpdatedMachineCount: 0,
+				},
+			},
+		},
+	}
+
+	require.Equal(t,
+		[]UpdatingPool{
+			{
+				Name:    "master",
+				Total:   3,
+				Updated: 2,
+			}, {
+				Name:    "x-storage",
+				Total:   3,
+				Updated: 0,
+			},
+		},
+		MachineConfigPoolsUpdating(subject),
+	)
+
+	require.Equal(t, "[master (2/3) x-storage (0/3)]", fmt.Sprint(MachineConfigPoolsUpdating(subject)))
 }
